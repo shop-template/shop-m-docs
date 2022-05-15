715a8b2ff7a11af167dbe69f5471a9e87aab3a85
fxss <723107234@qq.com>
2022-05-15 18:59:35 +0800

    feat: 配置 shop-m 使用文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+const { defaultTheme } = require('vuepress')
+const { searchPlugin } = require('@vuepress/plugin-search')
+
+module.exports = {
+  lang: 'zh-CN',
+  title: 'shop-m使用文档',
+  description: 'shop-m使用文档',
+  head: [['link', { rel: 'icon', href: '/images/logo.jpg' }]],
+  markdown: {
+  },
+  theme: defaultTheme({
+    // 默认主题配置
+    logo: '/images/logo.jpg',
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+    repo: 'shop-template/shop-m'
+  }),
+  plugins: [
+    searchPlugin({
+      // 配置项
+    }),
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+lang: zh-CN
+title: shop-m使用文档
+description: shop-m使用文档
+---
+
+# Hello VuePress
