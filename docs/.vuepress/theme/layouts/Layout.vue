ba8a3d81b5f19f7d9f926a2f3e693cabebd5ed0d
fxss <723107234@qq.com>
2022-05-17 10:37:19 +0800

    feat: 更精确的控制iframe

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,12 +1,87 @@
 <script setup>
+import { watch, ref, nextTick } from 'vue'
 import ParentLayout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
+import { useRouter, useRoute } from 'vue-router'
+import pathList from './../pathList.js'
+
+const route = useRoute()
+const iframeId = ref(null)
+const iframeBaseUrl = 'http://localhost:3000/shop-m/#'
+const iframeUrl = ref(iframeBaseUrl)
+
+//给子页面发消息
+function setChildrenInfo(path) {
+  iframeId.value && iframeId.value.contentWindow.postMessage(
+    {
+      cmd: 'setPath', 
+      res: {
+        path
+      }
+    },
+    '*'
+  )
+}
+
+// 根据父 path 拿到 子 path
+function parentPathToChildrenPath(parentPath) {
+  const cur = pathList.find(x => x.parentPath === parentPath)
+  return cur ? cur.childrenPath : ''
+}
+
+// 首次设置 iframe 的链接
+iframeUrl.value = `${iframeBaseUrl}${parentPathToChildrenPath(route.path)}`
+
+let osEnd = ref('pc')
+// 获取是移动端还是PC，摘自：https://tim.qq.com/
+const OS = function() {
+  var a = navigator.userAgent,
+      b = /(?:Android)/.test(a),
+      d = /(?:Firefox)/.test(a),
+      e = /(?:Mobile)/.test(a),
+      f = b && e,
+      g = b && !f,
+      c = /(?:iPad.*OS)/.test(a),
+      h = !c && /(?:iPhone\sOS)/.test(a),
+      k = c || g || /(?:PlayBook)/.test(a) || d && /(?:Tablet)/.test(a),
+      a = !k && (b || h || /(?:(webOS|hpwOS)[\s\/]|BlackBerry.*Version\/|BB10.*Version\/|CriOS\/)/.test(a) || d && e);
+  return {
+      android: b,
+      androidPad: g,
+      androidPhone: f,
+      ipad: c,
+      iphone: h,
+      tablet: k,
+      phone: a
+  }
+}();
+if (OS.phone || OS.ipad) {
+  osEnd.value = 'phone'
+}
+watch(
+  route,
+  async (val) => {
+    await nextTick()
+    const childrenPath = parentPathToChildrenPath(val.path)
+    if (childrenPath) setChildrenInfo(childrenPath)
+  },
+  {
+    deep: true,
+    immediate: true
+  }
+)
 </script>
 
 <template>
   <ParentLayout>
+    <!-- 因为page每次都重新渲染，所以pc端放在 navbar-after ，但移动端为了放置位置，所以放在 page-bottom -->
+    <template #navbar-after>
+      <div v-if="osEnd === 'pc'" class="docs-box">
+        <iframe :src="iframeUrl" frameborder="0" ref="iframeId"></iframe>
+      </div>
+    </template>
     <template #page-bottom>
-      <div class="docs-box">
-        <iframe src="https://shop-template.github.io/shop-m/#/" frameborder="0"></iframe>
+      <div v-if="osEnd === 'phone'" class="docs-box">
+        <iframe :src="iframeUrl" frameborder="0" ref="iframeId"></iframe>
       </div>
     </template>
   </ParentLayout>
---
+++ docs/.vuepress/theme/pathList.js
@@ -0,0 +1,15 @@
+// 配置父子 path
+export default [
+  {
+    parentPath: '/guide/',
+    childrenPath: '/list'
+  },
+  {
+    parentPath: '/guide/getting-started.html',
+    childrenPath: '/user'
+  },
+  {
+    parentPath: '/guide/tabbar.html',
+    childrenPath: '/'
+  }
+]
